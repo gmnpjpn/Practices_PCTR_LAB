40c5215f2426622dfd8ee4f43364c3763b80c04d
gmnpjpn <germanpajareropino@gmail.com>
2023-03-21 10:32:58 +0100

    Added comments

--- Practice_1_2_Functional/src/contador.c
+++ Practice_1_2_Functional/src/contador.c
@@ -6,38 +6,35 @@ void contarElementos(char *line,  int line_number);
 
 int main(int argc, char *argv[])
 {
-int numLinea=strtol(argv[2], NULL, 10); // Crea un long con 
+int numLinea=strtol(argv[2], NULL, 10); // Crea un long con el numero de linea en el que se encuentra
   
   contarElementos(argv[1],numLinea);
 
   return EXIT_SUCCESS;
 }
 
-void contarElementos(char *linea, int numero_linea)
+void contarElementos(char *linea, int numLinea) // Detecta si esta dentro o fuera de una palabra y las va contando
 {
   
-  int n_palabras = 0, dentro_palabra = 0;
-  const char *it = linea;
+  int numPalabras = 0, dentroPalabra = 0;
+  const char *iteracion = linea;
 
   do
   {
-    switch (*it)
+    switch (*iteracion)
     {
-    case '\0':
-    case ' ':
-    case '\t':
-    case '\n':
-    case '\r':
-      if (dentro_palabra)
-      {
-        dentro_palabra = 0;
-        n_palabras++;
-      }
-      break;
-    default:
-      dentro_palabra = 1;
-    }
-  } while (*it++);
 
-  printf("[CONTADOR %d] La linea '%d' tiene %d palabras\n", getpid(), numero_linea, n_palabras);
+    case '\r':  // En caso de que encuentre un retorno de carro
+      if (dentroPalabra)  // Si dentroPalabra esta a 1 (true) cuenta la palabra, pone a 0 dentroPalabra y continua ejecutando
+      {
+        dentroPalabra = 0;
+        numPalabras++;
+      }
+      break;  // Obliga a volver al do while
+    default:
+      dentroPalabra = 1;
+    }
+  } while (*iteracion++); // Mientras haya iteraciones posteriores se continua ejecutando
+
+  printf("[CONTADOR %d] La linea '%d' tiene %d palabras\n", getpid(), numLinea, numPalabras);
 }
