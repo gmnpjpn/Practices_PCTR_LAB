f7afbc73cc2f9a453c2f5e2b9b0aab25c25d7e68
DavidGarciaRivera <david.garcia4402@gmail.com>
2023-04-14 10:16:39 +0200

    algunas cosas que he visto que faltaban , sigue faltando cosas y estructurar.Estoy haciendo pruebas

--- Practice_2/src/telefono.c
+++ Practice_2/src/telefono.c
@@ -14,10 +14,25 @@ int main(int argc,char *argv[]){
 	//TODO: Esquema según especificado en la práctica.
 
     // Define variables locales
+    int pid;  // Identificador del proceso          
+    int valorEspera; // Número de llamadas en espera
+    srand(getpid());
+    
+
+
+
 
 
 
     // Coge semáforos y memoria compartida
+    crear_sem(MUTEXESPERA,1); 
+    crear_sem(TELEFONOS,0);
+    crear_sem(LINEAS,0);
+    
+
+    pid = getpid(); // Obtiene el identificador del proceso
+    valorEspera = leer_var(LLAMADASESPERA); // Obtiene el número de llamadas en espera
+
 
 
     
@@ -29,7 +44,13 @@ int main(int argc,char *argv[]){
         
 
 		//TODO: Aquí hay que realizar procesos
+        // Espera en espera
+        sleep(rand() % 10 + 10);
 
+        // Mensaje de llamada
+        printf("Teléfono [%d] recibe llamada...\n",pid);
+
+    
 
         // Mensaje de en conversacion
         printf("Teléfono [%d] en conversacion... Nº Llamadas en espera: %d\n",pid,valorEspera);
